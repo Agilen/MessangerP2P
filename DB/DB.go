e64654d6be51ce18a0be0b99277a5b98cbd1b269
fontan2312 <fontan2312@gmail.com>
2021-08-12 15:39:58 +0300

    Добавил историю сообщений

--- DB/DB.go
+++ DB/DB.go
@@ -1,8 +1,40 @@
 package db
 
-func AddNewPort(port int){
+import "database/sql"
 
+type Feed struct {
+	DB *sql.DB
 }
-func EditChatHistory(port int,chat string){
-	
+type T struct {
+	chatHistory string
+}
+
+// db, _ := sql.Open("sqlite3", "./ChatHistory.db")
+func NewFeed(db *sql.DB) *Feed {
+	stmt, _ := db.Prepare(`CREATE TABLE IF NOT EXISTS "ChatHistory" (
+		"ID"	INTEGER NOT NULL UNIQUE,
+		"port"	INTEGER NOT NULL UNIQUE,
+		"chatHistory"	TEXT,
+		PRIMARY KEY("id" AUTOINCREMENT)
+	);`)
+	stmt.Exec()
+	return &Feed{
+		DB: db,
+	}
+}
+
+func (feed *Feed) AddNewPort(port int) {
+	stmt, _ := feed.DB.Prepare(`INSERT INTO ChatHistory (port) values(?)`)
+	stmt.Exec(port)
+}
+func (feed *Feed) EditChatHistory(port int, chat string) {
+	text := feed.GetHistory(port)
+	text = text + chat
+	feed.DB.Exec("update ChatHistory set chatHistory=$1 where port=$2 ", text, port)
+}
+func (feed *Feed) GetHistory(port int) string {
+	var T T
+	Text := feed.DB.QueryRow("select chatHistory from ChatHistory where port=$2 ", port)
+	Text.Scan(&T.chatHistory)
+	return T.chatHistory
 }
